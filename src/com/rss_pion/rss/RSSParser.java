79d9587ac5dcff27969092e221d99c9c52bd9651
Whidou <whidou@gmail.com>
2014-02-01 21:55:56 +0100

    Complétion de RSSParser.

--- src/com/rss_pion/rss/RSSParser.java
+++ src/com/rss_pion/rss/RSSParser.java
@@ -2,8 +2,8 @@
  * @file    RSSParser.java
  * @author  PERROCHAUD Clément
  * @author  TOMA Hadrien
- * @date    2014-01-26
- * @version 0.2
+ * @date    2014-02-01
+ * @version 1.0
  *
  * Interpréteur pour flux RSS.
  ******************************************************************************/
@@ -13,7 +13,11 @@ package com.rss_pion.rss;
 /*** INCLUDES *****************************************************************/
 
 import com.rss_pion.beans.Article;
+import com.rss_pion.beans.CategoryArticle;
+import com.rss_pion.beans.CategoryFlux;
 import com.rss_pion.beans.Flux;
+import com.rss_pion.beans.Guid;
+import com.rss_pion.beans.TextInput;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -21,6 +25,9 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
@@ -41,8 +48,14 @@ public class RSSParser extends DefaultHandler {
     //! Variable temporaire pour la définition des articles
     private Article article;
 
+    //! Variable temporaire pour la définition des text inputs
+    private TextInput textInput;
+
     //! Buffer de lecture
     private StringBuilder text;
+
+    //! Format des dates
+    DateFormat dateFormat;
 
 /*** METHODS ******************************************************************/
 
@@ -56,6 +69,8 @@ public class RSSParser extends DefaultHandler {
         this.flux = new Flux();
         this.article = null;
         this.text = new StringBuilder();
+        this.dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy kk:mm:ss zzz",
+                Locale.US);
     }
 
 /***************************************************************************//**
@@ -120,6 +135,15 @@ public class RSSParser extends DefaultHandler {
             // Ajout de l'article au flux
             this.flux.addArticle(this.article);
 
+        // À l'ouverture d'une balise de texr input
+        } else if (qName.equalsIgnoreCase("textinput")) {
+
+            // Création du text input
+            this.textInput = new TextInput();
+
+            // Ajout de l'input au flux
+            this.flux.setTextInput(this.textInput);
+
         // À l'ouverture d'une balise de GUID d'article
         } else if (qName.equalsIgnoreCase("guid") && this.article != null) {
 
@@ -147,45 +171,7 @@ public class RSSParser extends DefaultHandler {
         
         String value = this.text.toString().trim();
 
-        if (this.article == null) {
-
-            // Attributs du flux
-            if (qName.equalsIgnoreCase("category")) {
-                this.flux.addCategory(value);
-            } else if (qName.equalsIgnoreCase("copyright")) {
-                this.flux.setCopyright(value);
-            } else if (qName.equalsIgnoreCase("description")) {
-                this.flux.setDescription(value);
-            } else if (qName.equalsIgnoreCase("generator")) {
-                this.flux.setGenerator(value);
-            } else if (qName.equalsIgnoreCase("language")) {
-                this.flux.setLanguage(value);
-            } else if (qName.equalsIgnoreCase("lastbuilddate")) {
-                this.flux.setLastBuildDate(value);
-            } else if (qName.equalsIgnoreCase("link")) {
-                this.flux.setLink(value);
-            } else if (qName.equalsIgnoreCase("managingeditor")) {
-                this.flux.setManagingEditor(value);
-            } else if (qName.equalsIgnoreCase("pubdate")) {
-                this.flux.setPubDate(value);
-            } else if (qName.equalsIgnoreCase("rating")) {
-                this.flux.setRating(value);
-            } else if (qName.equalsIgnoreCase("skipdays")) {
-                this.flux.setSkipDays(value);
-            } else if (qName.equalsIgnoreCase("skiphours")) {
-                this.flux.setSkipHours(value);
-            } else if (qName.equalsIgnoreCase("textinput")) {
-                this.flux.setTextInput(value);
-            } else if (qName.equalsIgnoreCase("title")) {
-                this.flux.setTitle(value);
-            } else if (qName.equalsIgnoreCase("ttl")) {
-                this.flux.setTtl(value);
-
-            // Attributs de l'image
-            } else if (qName.equalsIgnoreCase("url")) {
-                this.flux.setUrlImage(value);
-            }
-        } else {
+        if (this.article != null) {
 
             // Fin de l'article
             if (qName.equalsIgnoreCase("item")) {
@@ -195,7 +181,7 @@ public class RSSParser extends DefaultHandler {
             } else if (qName.equalsIgnoreCase("author")) {
                 this.article.setAuthor(value);
             } else if (qName.equalsIgnoreCase("category")) {
-                this.article.addCategory(value);
+                this.article.addCategory(new CategoryArticle(value));
             } else if (qName.equalsIgnoreCase("comments")) {
                 this.article.setComments(value);
             } else if (qName.equalsIgnoreCase("description")) {
@@ -207,11 +193,63 @@ public class RSSParser extends DefaultHandler {
             } else if (qName.equalsIgnoreCase("link")) {
                 this.article.setLink(value);
             } else if (qName.equalsIgnoreCase("pubDate")) {
-                this.article.setPubDate(value);
+                this.article.setPubDate(dateFormat.parse(value).getTime());
             } else if (qName.equalsIgnoreCase("source")) {
                 this.article.setSource(value);
             } else if (qName.equalsIgnoreCase("title")) {
                 this.article.setTitle(value);
+            }
+        } else if (this.textInput != null) {
+
+            // Fin de l'input
+            if (qName.equalsIgnoreCase("textinput")) {
+                this.textInput = null;
+
+            // Attributs de l'input
+            } else if (qName.equalsIgnoreCase("description")) {
+                this.textInput.setDescription(value);
+            } else if (qName.equalsIgnoreCase("name")) {
+                this.textInput.setName(value);
+            } else if (qName.equalsIgnoreCase("link")) {
+                this.textInput.setLink(value);
+            } else if (qName.equalsIgnoreCase("title")) {
+                this.textInput.setTitle(value);
+            }
+        } else {
+
+            // Attributs du flux
+            if (qName.equalsIgnoreCase("category")) {
+                this.flux.addCategory(new CategoryFlux(value));
+            } else if (qName.equalsIgnoreCase("copyright")) {
+                this.flux.setCopyright(value);
+            } else if (qName.equalsIgnoreCase("day")) {
+                this.flux.addSkipDay(value);
+            } else if (qName.equalsIgnoreCase("description")) {
+                this.flux.setDescription(value);
+            } else if (qName.equalsIgnoreCase("generator")) {
+                this.flux.setGenerator(value);
+            } else if (qName.equalsIgnoreCase("hour")) {
+                this.flux.addSkipHour(Integer.parseInt(value));
+            } else if (qName.equalsIgnoreCase("language")) {
+                this.flux.setLanguage(value);
+            } else if (qName.equalsIgnoreCase("lastbuilddate")) {
+                this.flux.setLastBuildDate(dateFormat.parse(value).getTime());
+            } else if (qName.equalsIgnoreCase("link")) {
+                this.flux.setLink(value);
+            } else if (qName.equalsIgnoreCase("managingeditor")) {
+                this.flux.setManagingEditor(value);
+            } else if (qName.equalsIgnoreCase("pubdate")) {
+                this.flux.setPubDate(dateFormat.parse(value).getTime());
+            } else if (qName.equalsIgnoreCase("rating")) {
+                this.flux.setRating(value);
+            } else if (qName.equalsIgnoreCase("title")) {
+                this.flux.setTitle(value);
+            } else if (qName.equalsIgnoreCase("ttl")) {
+                this.flux.setTtl(Integer.parseInt(value));
+
+            // Attributs de l'image
+            } else if (qName.equalsIgnoreCase("url")) {
+                this.flux.setUrlImage(value);
             }
         }
 
